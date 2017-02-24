3de6b2a2b0c6eb6b0681f43ec99e2903e42e852d
happyendik <vlom4ek@gmail.com>
2017-02-24 16:51:07 +0300

    commit through PHPstorm

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php 
 
-echo "delete";
-
-<<<<<<< HEAD
-
-=======
-echo "test second commit";
-echo "new branch";
-echo "new string";
->>>>>>> 25702824b41d3b8f5d9e7859cb5004772c990576
+echo "PHPStorm";
